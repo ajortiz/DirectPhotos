86a9996436469e3333fdab1629e9a946668f6a81
aortiz <contactingaortiz@gmail.com>
2017-11-06 15:21:53 -0600

    YESSSSS created project for testing uploading photos / files to firebase

---
+++ DirectPhotos/DirectPhotos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DirectPhotos
+//
+//  Created by Amanda Ortiz on 11/6/17.
+//  Copyright © 2017 aortiz6. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
