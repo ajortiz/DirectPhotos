1d58a5bde864b3c5ef0ed6f251c339a40ec97958
aortiz <contactingaortiz@gmail.com>
2018-02-17 14:28:20 -0600

    added helper methods

--- directPhotos_Incubator/phpMySQLTest/Helper.swift
+++ directPhotos_Incubator/phpMySQLTest/Helper.swift
@@ -10,7 +10,69 @@
 import UIKit
 
 class Helper: UIViewController {
-
-   
+    let URL_SAVE_NEW_MEMBER = "http://www.aortiz6.create.stedwards.edu/directPhotosTEST/createUser.php?"
+    let URL_SAVE_GET_MEMBER = "http://www.aortiz6.create.stedwards.edu/directPhotosTEST/loginUser.php?"
+    let URL_CREATE_NEW_ALBUM = "http://www.aortiz6.create.stedwards.edu/directPhotosTEST/createAlbum.php?"
+    let URL_GET_ALBUM = "http://www.aortiz6.create.stedwards.edu/directPhotosTEST/getAlbum.php?"
+    
+    
+    
+    func createEmailAccount(emailToRegister: String) {
+        //created NSURL
+        let requestURL = NSURL(string: URL_SAVE_NEW_MEMBER)
+        
+        //creating NSMutableURLRequest
+        let request = NSMutableURLRequest(url: requestURL! as URL)
+        
+        //setting the method to post
+        request.httpMethod = "POST"
+        
+        //getting values from text fields
+        //let newEmail=newEmailText.text
+        //let newPassword = newPasswordText.text
+        
+        //creating the post parameter by concatenating the keys and values from text field
+        let postParameters = "email="+emailToRegister
+        
+        //adding the parameters to request body
+        request.httpBody = postParameters.data(using: String.Encoding.utf8)
+        
+        
+        //creating a task to send the post request
+        let task = URLSession.shared.dataTask(with: request as URLRequest){
+            data, response, error in
+            
+            if error != nil{
+                print("error is \(String(describing: error))")
+                return;
+            }
+            
+            //parsing the response
+            do {
+                //converting resonse to NSDictionary
+                let myJSON =  try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? NSDictionary
+                
+                //parsing the json
+                if let parseJSON = myJSON {
+                    
+                    //creating a string
+                    var msg : String!
+                    
+                    //getting the json response
+                    msg = parseJSON["message"] as! String?
+                    
+                    //printing the response
+                    print(msg)
+                    
+                }
+            } catch {
+                print(error)
+            }
+            
+        }
+        //executing the task
+        task.resume()
+    }
+    
 
 }
--- directPhotos_Incubator/phpMySQLTest/ViewController.swift
+++ directPhotos_Incubator/phpMySQLTest/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
     let URL_CREATE_NEW_ALBUM = "http://www.aortiz6.create.stedwards.edu/directPhotosTEST/createAlbum.php?"
      let URL_GET_ALBUM = "http://www.aortiz6.create.stedwards.edu/directPhotosTEST/getAlbum.php?"
 
-    
+    //let HelperClass = new Helper()
     //TextFields declarations
   //  @IBOutlet weak var newEmailText: UITextField!
     //@IBOutlet weak var newPasswordText: UITextField!
@@ -97,63 +97,7 @@ class ViewController: UIViewController {
 // ----- CREATE NEW ACCOUNT --------
     
     //SAVE Button action method
-    func createEmailAccount(emailToRegister: String) {
-        //created NSURL
-        let requestURL = NSURL(string: URL_SAVE_NEW_MEMBER)
-        
-        //creating NSMutableURLRequest
-        let request = NSMutableURLRequest(url: requestURL! as URL)
-        
-        //setting the method to post
-        request.httpMethod = "POST"
-        
-        //getting values from text fields
-        //let newEmail=newEmailText.text
-        //let newPassword = newPasswordText.text
-        
-        //creating the post parameter by concatenating the keys and values from text field
-        let postParameters = "email="+emailToRegister
-        
-        //adding the parameters to request body
-        request.httpBody = postParameters.data(using: String.Encoding.utf8)
-        
-        
-        //creating a task to send the post request
-        let task = URLSession.shared.dataTask(with: request as URLRequest){
-            data, response, error in
-            
-            if error != nil{
-                print("error is \(String(describing: error))")
-                return;
-            }
-            
-            //parsing the response
-            do {
-                //converting resonse to NSDictionary
-                let myJSON =  try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? NSDictionary
-                
-                //parsing the json
-                if let parseJSON = myJSON {
-                    
-                    //creating a string
-                    var msg : String!
-                    
-                    //getting the json response
-                    msg = parseJSON["message"] as! String?
-                    
-                    //printing the response
-                    print(msg)
-                    
-                }
-            } catch {
-                print(error)
-            }
-            
-        }
-        //executing the task
-        task.resume()
-    }
- 
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -191,7 +135,7 @@ class ViewController: UIViewController {
         let emailAddress=emailAddressText.text
         let newKey=generateKey(length: 4)
         
-        createEmailAccount(emailToRegister: emailAddress!)
+        Helper().createEmailAccount(emailToRegister: emailAddress!)
         print(newKey)
         
         
